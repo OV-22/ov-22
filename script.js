698f498e14e87d02da09351a66113b70823b08b6
Avishekghorai <avishekghorai22@gmail.com>
2026-01-03 19:52:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-// Dark Mode Toggle
+// Dark Mode
 const toggle = document.getElementById("themeToggle");
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-  toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+  toggle.textContent =
+    document.body.classList.contains("dark") ? "☀️" : "🌙";
 });
 
-// Skill Animation on Scroll
-const skills = document.querySelectorAll(".progress div");
+// Skill Animation
+const bars = document.querySelectorAll(".bar div");
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
@@ -16,4 +17,4 @@ const observer = new IntersectionObserver(entries => {
   });
 }, { threshold: 0.5 });
 
-skills.forEach(skill => observer.observe(skill));
+bars.forEach(bar => observer.observe(bar));
